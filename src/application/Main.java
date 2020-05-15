ae7c62897505ff6bb9dd97b032404f3d9c8d4ae2
amagalhaes31 <alexandremagalhaes31@gmail.com>
2020-05-15 17:44:01 -0300

    Projeto criado

---
+++ src/application/Main.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author alexandre
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws ParseException 
+	 */
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+		//Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		Date d = new Date(System.currentTimeMillis());
+		
+		// Imprime o valor da variável "d"
+		System.out.println(sdf.format(d));
+		
+		// Cria a variável "cal" do tipo Calendar
+		Calendar cal = Calendar.getInstance();
+		
+		// Passa o valor de "d" para "cal"
+		cal.setTime(d);
+		
+		// Adiciona "4" horas
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		
+		// Pega o valor atualizado
+		d = cal.getTime();
+		
+		// Imprime o valor da variável "d" atualizado
+		System.out.println(sdf.format(d));
+		
+	}
+
+}
